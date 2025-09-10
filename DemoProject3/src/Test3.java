93d5be5f6b9a5ebdd32cc51aada2a67917f5f468
shivpokale <168342897+shivpokale@users.noreply.github.com>
2025-09-10 16:32:20 +0530

    Update Test3.java

--- DemoProject3/src/Test3.java
+++ DemoProject3/src/Test3.java
@@ -3,6 +3,7 @@ public class Test3 {
 	public static void main(String[] args) {
 		System.out.println("Statement 1");
 		System.out.println("Statement 2");
+		System.out.println("Statement 3");
 		
 	}
 
